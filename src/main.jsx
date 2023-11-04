8885003d691ea9d17275346cb0ddd3b513392707
Edmon <ed.moussa4@gmail.com>
2023-11-04 17:08:31 +0300

    added browser router

--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,16 @@ import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./app/store.js";
 import { extendedApiSlice } from "./features/movies/moviesSlice.js";
+import { BrowserRouter } from "react-router-dom";
 
 store.dispatch(extendedApiSlice.endpoints.discoverMovies.initiate());
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
