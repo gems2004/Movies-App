343d7979611851c992ad452b93c8ae3170bb9997
Edmon <ed.moussa4@gmail.com>
2023-11-04 17:10:03 +0300

    added some endpoints

--- src/features/movies/moviesSlice.js
+++ src/features/movies/moviesSlice.js
@@ -12,12 +12,34 @@ export const extendedApiSlice = apiSlice.injectEndpoints({
         `/discover/movie?include_adult=false&language=en-US&page=1&sort_by=popularity.desc`,
       providesTags: (result, error, arg) =>
         result
-          ? [...result.results.map(({ id }) => ({ type: "Post", id })), "Post"]
-          : ["Post"],
+          ? [...result.results.map(({ id }) => ({ type: "Show", id })), "Show"]
+          : ["Show"],
+    }),
+    getMovieById: builder.query({
+      query: (id) => `/movie/${id}?language=en-US`,
+    }),
+    getPopularMovies: builder.query({
+      query: () => `/movie/popular?language=en-US&page=1`,
+      providesTags: (result, error, arg) =>
+        result
+          ? [...result.results.map(({ id }) => ({ type: "Show", id })), "Show"]
+          : ["Show"],
+    }),
+    getUpcomingMovies: builder.query({
+      query: () => "/movie/upcoming?language=en-US&page=1",
+      providesTags: (result, error, arg) =>
+        result
+          ? [...result.results.map(({ id }) => ({ type: "Show", id })), "Show"]
+          : ["Show"],
     }),
   }),
 });
-export const { useDiscoverMoviesQuery } = extendedApiSlice;
+export const {
+  useDiscoverMoviesQuery,
+  useGetMovieByIdQuery,
+  useGetPopularMoviesQuery,
+  useGetUpcomingMoviesQuery,
+} = extendedApiSlice;
 
 export const selectMoviesResult =
   extendedApiSlice.endpoints.discoverMovies.select();
