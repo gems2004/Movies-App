e9c3f6c40e27689c9f4678be22cfbf4c8c5856d5
Edmon <ed.moussa4@gmail.com>
2023-11-07 17:33:41 +0300

    removed comment

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const movieApi = createApi({
-  reducerPath: "movieApi", // default value
+  reducerPath: "movieApi",
   baseQuery: fetchBaseQuery({
     baseUrl: process.env.VITE_MOVIES_URL,
     prepareHeaders: (headers) => {
